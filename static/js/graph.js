414ada74e5175440cbf8280a6ced2caffbf88e04
Ubuntu <ubuntu@ip-172-31-86-208.ec2.internal>
2019-07-25 12:41:08 +0000

    added average age

--- static/js/graph.js
+++ static/js/graph.js
@@ -11,7 +11,8 @@ function makeGraphs(error, healthData) {
         d.age = parseInt(d.age);
     })
 
-    /* show_average_age(ndx, gender, element); */
+    show_average_age(ndx);
+    show_average_age_gender(ndx, "Female", "#average_age_gender");
 
     show_select_company(ndx);
     show_gender_breakdown(ndx);
@@ -72,47 +73,81 @@ function show_treatment_levels(ndx) {
         .xUnits(dc.units.ordinal)
 }
 
-/* function show_average_age(ndx, gender, element) {
+function show_average_age(ndx) {
+    var dim = ndx.dimension(dc.pluck('gender'));
 
-    var gender_dim = ndx.dimension(dc.pluck("age"));
-    var average_age = gender_dim.group().reduce(
+    function add_item(p, v) {
+        p.count++;
+        p.total += v.age;
+        p.average = p.total / p.count;
+        return p;
+    }
 
-        function(p, v) {
-            p.count++;
-            p.total += v.age;
+    function remove_item(p, v) {
+        p.count--;
+        if (p.count == 0) {
+            p.total = 0;
+            p.average = 0;
+        }
+        else {
+            p.total -= v.age;
             p.average = p.total / p.count;
-            return p;
-        },
+        }
+        return p;
+    }
 
+    function initialise() {
+        return { count: 0, total: 0, average: 0 };
+    }
+
+    var averageAgeByGender = dim.group().reduce(add_item, remove_item, initialise);
+    dc.barChart("#average_age")
+        .width(400)
+        .height(300)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .dimension(dim)
+        .group(averageAgeByGender)
+        .valueAccessor(function(d) {
+            return d.value.average.toFixed(2);
+        })
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("Gender")
+        .yAxis().ticks(4);
+}
+
+function show_average_age_gender(ndx, gender, element) {
+    var averageAgeByGender = ndx.groupAll().reduce(
         function(p, v) {
-            p.count--;
-            if (p.count == 0) {
-                p.total = 0;
-                p.average = 0;
-            }
-            else {
-                p.total -= v.age;
-                p.average = p.total / p.count;
+            if (v.gender === "Female") {
+                p.count++;
+                if (v.age === true) {
+                    p.count++;
+                    p.total += v.age;
+                    p.average = p.total / p.count;
+                }
             }
             return p;
         },
-
+        function(p, v) {
+            if (v.gender === "Female") {
+                p.count--;
+                if (v.age === true) {
+                    p.count--;
+                    p.total += v.age;
+                    p.average = p.total / p.count;
+                }
+                else {
+                    p.total -= v.age;
+                    p.average = p.total / p.count;
+                }
+            }
+            return p;
+        },
         function() {
             return { count: 0, total: 0, average: 0 };
-        }
+        },
     );
-
-    dc.numberDisplay(element)
-        .formatNumber(d3.format(".2"))
-        .valueAccessor(function(d) {
-            if (d.count == 0) {
-                return 0;
-            }
-            else {
-                return (d.age / d.count);
-            }
-        })
-        .group(age_dim)
-} 
-
-*/
+}
